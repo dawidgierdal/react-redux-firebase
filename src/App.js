b2ba1d5b7231fa9f42b12fcca79ff1d3c51cdd77
Dawid Gierdal <dawid.gierdal@mobilereality.pl>
2019-05-20 16:48:10 +0200

    withAuthentication HOC

--- src/App.js
+++ src/App.js
@@ -9,41 +9,19 @@ import { LandingPage } from "./components/Landing";
 import { HomePage } from "./components/Home";
 import { SignUpPage } from "./components/SignUp";
 import { SignInPage } from "./components/SignIn";
-import { withFirebase } from "./components/Firebase";
+import withAuthentication from './components/Session/withAuthentication'
 
-class App extends React.Component {
-    state = {
-        authUser: null,
-    };
-    componentDidMount() {
-        this.listener = this.props.firebase.auth.onAuthStateChanged(
-            authUser => {
-                authUser
-                    ? this.setState({ authUser })
-                    : this.setState({ authUser: null });
-            },
-        );
-    }
+const App = () => (
+    <Router>
+        <div>
+            <Navigation />
+            <hr />
+            <Route exact path={PATH.LANDING} component={LandingPage} />
+            <Route path={PATH.SIGN_UP} component={SignUpPage} />
+            <Route path={PATH.SIGN_IN} component={SignInPage} />
+            <Route path={PATH.HOME} component={HomePage} />
+        </div>
+    </Router>
+);
 
-    componentWillUnmount() {
-        this.listener();
-    }
-
-    render() {
-        return (
-            <div className="App">
-                <Router>
-                    <Navigation authUser={this.state.authUser} />
-                    <div>
-                        <Route exact path={PATH.LANDING} component={LandingPage} />
-                        <Route path={PATH.HOME} component={HomePage} />
-                        <Route path={PATH.SIGN_UP} component={SignUpPage} />
-                        <Route path={PATH.SIGN_IN} component={SignInPage} />
-                    </div>
-                </Router>
-            </div>
-        );
-    }
-}
-
-export default withFirebase(App);
+export default withAuthentication(App);
